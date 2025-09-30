a1ff7c3474c13b787bd51a4ec99b466273eeb90b
unknown <node1@flyweis.technology>
2025-09-30 19:38:26 +0530

    set all

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,6 +1,10 @@
+// 
+const KeId = "AKIA455B2ZFDHIMVQZ4U";
+const SKey = "Ty00K8ma0FPM2CQMcbbDM3cAqaXYhMWryNrBDTOX";
+const Reg = "ap-south-1";
 
-const KeyId = "AKIA455B2ZFDHIMVQZ4U";
-const SecretKey = "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";
-const Region = "ap-south-1";
+// const KeyId = "";
+// const SecretKey = "";
+// const Region = "";
 
-module.exports = { KeyId, SecretKey, Region };
+module.exports = { KeId, SKey, Reg };
--- src/routes/Master/file_upload.routes.js
+++ src/routes/Master/file_upload.routes.js
@@ -3,19 +3,14 @@ const busboy = require('connect-busboy');
 const { uploadToS3 } = require('../../utils/s3Upload');
 
 const router = express.Router();
-
-// Middleware for busboy
 router.use(busboy());
 
-// POST /upload - file upload to S3  2025-07-17
 router.post('/', (req, res) => {
   req.pipe(req.busboy);
   req.busboy.on('file', async (fieldname, file, filename, encoding, mimetype) => {
     try {
-      // console.log('Upload details:', { fieldname, filename, encoding, mimetype });
-      // Ensure we have a valid filename
-      const validFilename = filename || `${fieldname}_${Date.now()}`;
       
+      const validFilename = filename || `${fieldname}_${Date.now()}`;
       const fileUrl = await uploadToS3(file, validFilename);
       res.status(200).json({ 
         success: true, 
@@ -25,7 +20,6 @@ router.post('/', (req, res) => {
         mimetype: mimetype
       });
     } catch (err) {
-      // console.error('Upload error:', err);
       res.status(500).json({ success: false, message: err.message });
     }
   });
