fc1eda4d6ef20a1d0263180392867206fd15b020
unknown <node1@flyweis.technology>
2025-09-30 19:39:13 +0530

    set allss

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,10 +1,7 @@
-// 
-const KeId = "AKIA455B2ZFDHIMVQZ4U";
-const SKey = "Ty00K8ma0FPM2CQMcbbDM3cAqaXYhMWryNrBDTOX";
-const Reg = "ap-south-1";
 
-// const KeyId = "";
-// const SecretKey = "";
-// const Region = "";
 
-module.exports = { KeId, SKey, Reg };
+const KeyId = "";
+const SecretKey = "";
+const Region = "";
+
+module.exports = { KeyId, SecretKey, Region };
