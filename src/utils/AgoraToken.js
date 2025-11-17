f5c3edee386526429deee9c4688ade2c702a1d9b
node1 <node1@flyweis.technology>
2025-11-17 16:48:52 +0530

    udpate set

--- src/utils/AgoraToken.js
+++ src/utils/AgoraToken.js
@@ -39,7 +39,7 @@ console.log(appId, appCertificate, channelName,userId, advisorId, advisoreRole, 
         const userToken = RtcTokenBuilder.buildTokenWithUid(
             appId,
             appCertificate,
-            channelName,
+            "easyAdvisingToken",
             userId,
             userRole,
             privilegeExpirationInSecond
@@ -48,7 +48,7 @@ console.log(appId, appCertificate, channelName,userId, advisorId, advisoreRole, 
         const advisorToken = RtcTokenBuilder.buildTokenWithUid(
             appId,
             appCertificate,
-            channelName,
+            "easyAdvisingToken",
             advisorId,
             advisoreRole,
             privilegeExpirationInSecond
